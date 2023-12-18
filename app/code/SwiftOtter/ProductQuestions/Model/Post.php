f9be9db62c267c59c2d34332ddc43cf23caa99be
AnimNyan <tellm3pls@gmail.com>
2023-12-18 17:56:26 +1100

    Finished 3.3

--- app/code/SwiftOtter/ProductQuestions/Model/Post.php
+++ app/code/SwiftOtter/ProductQuestions/Model/Post.php
@@ -3,8 +3,9 @@ declare(strict_types=1);
 
 namespace SwiftOtter\ProductQuestions\Model;
 
+use Magento\Framework\DataObject\IdentityInterface;
 use Magento\Framework\Model\AbstractModel;
-class Post extends AbstractModel
+class Post extends AbstractModel implements IdentityInterface
 {
     //inheritdoc is for overriding or implementing a method from a base class or interface
     /**
@@ -17,4 +18,12 @@ class Post extends AbstractModel
             \SwiftOtter\ProductQuestions\Model\ResourceModel\Post::class
         );
     }
+
+    public function getIdentities()
+    {
+        //we attach the post object to our product before saving it
+        $product = $this->getProduct();
+        return ($product) ? $product->getIdentities() : [];
+    }
+
 }
--- app/code/SwiftOtter/ProductQuestions/ViewModel/ProductView/Questions.php
+++ app/code/SwiftOtter/ProductQuestions/ViewModel/ProductView/Questions.php
@@ -6,24 +6,69 @@ namespace SwiftOtter\ProductQuestions\ViewModel\ProductView;
 //used to fetch Store Config values
 //ScopeConfigInterface.getValue and ScopeConfigInterface.isSetFlag cast as boolean
 //can fetch on scope basis
+use Magento\Catalog\Model\Product;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use SwiftOtter\ProductQuestions\Model\Config;
 
+use SwiftOtter\ProductQuestions\Model\ResourceModel\Post\Collection as PostCollection;
+use SwiftOtter\ProductQuestions\Model\ResourceModel\Post\CollectionFactory as PostCollectionFactory;
+
+use Magento\Framework\Registry;
+
 
 class Questions implements ArgumentInterface
 {
+    //caching it the first time we fetch it
+    private ?Product $product = null;
     private Config $config;
+    private PostCollectionFactory $postCollectionFactory;
+    private Registry $coreRegistry;
 
     public function __construct(
-        Config $config
+        Config $config,
+        PostCollectionFactory $postCollectionFactory,
+        Registry $coreRegistry
     ) {
         $this->config = $config;
+        $this->postCollectionFactory = $postCollectionFactory;
+        $this->coreRegistry = $coreRegistry;
     }
 
     public function getHeading(): string
     {
         return $this->config->getHeading();
+    }
+
+    public function getQuestions(): array
+    {
+        $product = $this->getProduct();
+
+        //if we didn't get back a product
+        if (!$product) {
+            return [];
+        }
+
+        /** @var PostCollection $posts */
+        $posts = $this->postCollectionFactory->create();
+        //don't trust type
+        //we only want questions
+        $posts->addProductIdFilter((int) $product->getId())
+            ->addQuestionsOnlyFilter()
+            ->addAnswers()
+            ->addOrder('created_at', 'DESC');
+
+        //collection model $posts we could pass that straight to an iterator
+        //we are calling getItems() to explicitly get that array
+        return $posts->getItems();
+    }
+
+    private function getProduct(): ?Product
+    {
+        if ($this->product === null) {
+            $this->product = $this->coreRegistry->registry('product');
+        }
+        return $this->product;
     }
 }
 
